a808fd30d692f30985b8f86bbbd5fc8f8db985ca
shosokaw <shosokaw@C010423556.zipangu.cac.co.jp>
2026-04-16 14:12:18 +0900

    Question06完了

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_1.java
@@ -6,7 +6,11 @@ public class Question06_1 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		String[] massage = { "こんにちは", "Java学習中", "繰り返しの演習", "頑張ります" };
+
+		for (int i = 0; i < 4; i++) {
+			System.out.println(massage[i]);
+		}
+		System.out.println("繰り返しが完了しました。");
 	}
 }
